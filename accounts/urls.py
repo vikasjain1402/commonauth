142d486a1ef64f2f7adef60ce431e6b82644a4da
vikas <vikasjain1402@gmail.com>
2020-11-08 15:44:11 +0530

    url for ligin line added

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,6 +20,7 @@ from . import views
 urlpatterns = [
 
 path("",views.login,name="login"),
+path("login/",views.login,name="login"),
 path("signup/",views.signup,name="signup"),
 path("logout/",views.logout,name="logout"),
 path("update/",views.update,name="update")
